10834142eb7234d5c3da357bab238cef04ca978a
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-08-14 13:50:48 +0200

    hello

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,5 +1,5 @@
 import Heading from "@components/heading";
 
 export default function About() {
-  return <Heading>About TESTING TESTING FEATURE BRANCH</Heading>;
+  return <Heading>About no more testing</Heading>;
 }
